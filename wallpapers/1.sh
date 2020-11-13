4b41640a4fcbb2a6cc9adfc404d96137093d5407
rainboy <rainboylvx@qq.com>
2020-11-13 09:52:28 +0800

    new wp

--- wallpapers/1.sh
+++
@@ -1,2 +0,0 @@
-convert wallhaven-103179.png -resize $(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/') RGB:- 
-#| i3lock --raw $(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/'):rgb --image /dev/stdin
